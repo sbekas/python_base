340b0b91e890083b03b73976be882ad0d5634296
stbekas <stbekas@gmail.com>
2022-03-07 00:38:34 +0300

    Added classwork to lesson 4

--- lesson_4/home/task6/int_num_list_generator.py
+++
@@ -1,9 +0,0 @@
-
-from itertools import count
-
-def int_generator(start_num, end_num):
-    for el in count(start_num):
-        if el > end_num:
-            break
-        else:
-            print(el)
